4e91d71d468e47c5e4220e5e57c00f36722ce113
ferdelance0 <doncdelish0@gmail.com>
2024-04-02 23:08:06 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const firebase = require("firebase/app"); // only require firebase/app
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1dVS7n0430QQt8G6IfSZ4p5YICTPJpT4",
+    authDomain: "simpledfileupload.firebaseapp.com",
+    projectId: "simpledfileupload",
+    storageBucket: "simpledfileupload.appspot.com",
+    messagingSenderId: "587580470807",
+    appId: "1:587580470807:web:cf7b8297413769fb9ece52",
+    measurementId: "G-P5W2E7LMVT"
+  };
+// Initialize Firebase
+firebaseapp= firebase.initializeApp(firebaseConfig);
+
+module.exports = firebaseapp;
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const firebase = require("./firebase"); // Import Firebase instance
+const {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} = require("firebase/auth");
+const auth = getAuth(firebase);
+const app = express();
+app.use(express.json());
+
+// Signup route
+app.post("/signup", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const userCredential = await createUserWithEmailAndPassword(auth,email,password);
+        res.status(201).send("User signed up successfully");
+
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+
+});
+
+// Login route
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userCredential = await signInWithEmailAndPassword(auth,email, password);
+    res.status(200).send("Login successful");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
